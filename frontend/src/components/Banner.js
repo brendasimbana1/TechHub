0cc5a134ffce68b8927d0468c65f78457bbc548c
brendasimbana1 <brendasimbanag1@est.ups.edu.ec>
2026-01-16 15:36:00 -0500

    Diseño de Banner

---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h1>TechHub</h1>
+        <p>Gestiona tutorias de la carrera de Computación fácilmente.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
